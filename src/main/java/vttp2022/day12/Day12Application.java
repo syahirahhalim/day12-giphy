beda9cdd0c890f5032efb5fa2fb1fffc6a436bdd
syahirahhalim <syahawesome@gmail.com>
2022-04-05 11:05:00 +0800

    initial commit

---
+++ src/main/java/vttp2022/day12/Day12Application.java
@@ -0,0 +1,13 @@
+package vttp2022.day12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day12Application.class, args);
+	}
+
+}
---
+++ src/test/java/vttp2022/day12/Day12ApplicationTests.java
@@ -0,0 +1,13 @@
+package vttp2022.day12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Day12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
